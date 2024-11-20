dcdea18085a0bba0158f0215ebdea8aafc85381a
andre mcmorris <ajmcmorris@gmail.com>
2024-11-19 21:14:58 -0500

    added caching to the session to reduce api calls, added new service method,added websocket service

--- src/app/services/stock/stock.service.ts
+++ src/app/services/stock/stock.service.ts
@@ -2,26 +2,46 @@ import { inject, Injectable } from '@angular/core';
 import { envrionment } from '../../../../environments/environment';
 import { COMPANY_PROFILE } from '../../types/stock.const';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StockService {
   key = envrionment.stockApiKey;
+  baseUrl = 'https://www.alphavantage.co/query';
   http = inject(HttpClient);
+  private cache = new Map<string, any>();
   constructor() {}
+
+  getStockData(symbol: string): Observable<any> {
+    const cacheKey = `stock-${symbol}`;
+    const cachedData = this.cache.get(cacheKey);
+    if (cachedData) {
+      return cachedData;
+    }
+
+    return this.http
+      .get(
+        `${this.baseUrl}?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${this.key}`
+      )
+      .pipe(
+        tap((data) => {
+          this.cache.set(cacheKey, data); //cache the data
+        })
+      );
+  }
 
   getCompanyDetails(symbol: string): Observable<any> {
     console.log(symbol);
     return this.http.get(
-      `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=${this.key}`
+      `${this.baseUrl}?function=OVERVIEW&symbol=${symbol}&apikey=${this.key}`
     );
   }
 
-  getETF(symbol: string, profile: COMPANY_PROFILE) {
-    return fetch(
-      `https://www.alphavantage.co/query?function=${profile}&symbol=${symbol}&apikey=${this.key}`
+  getETF(symbol: string, profile: COMPANY_PROFILE): Observable<any> {
+    return this.http.get(
+      `${this.baseUrl}?function=${profile}&symbol=${symbol}&apikey=${this.key}`
     );
   }
 }
--- src/app/services/websocket/websocket.service.ts
+++ src/app/services/websocket/websocket.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { StockService } from '../stock/stock.service';
+import { map, switchMap } from 'rxjs/operators';
+import { interval } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class WebsocketService {
+  private stockService = inject(StockService);
+  constructor() {}
 
-  constructor() { }
+  getLiveStockData(symbol: string) {
+    return interval(1000).pipe(
+      //emit every second
+      switchMap(() => this.stockService.getStockData(symbol)),
+      map((data) => {
+        return data['Time Series (Daily)'];
+      })
+    );
+  }
 }
